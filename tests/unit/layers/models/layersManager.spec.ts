0c5bd2dd7587aa62588e3a19e4c6463a8213691f
shlomiko <cl-shlomikoncha@rnd-hub.com>
2021-01-19 11:45:08 +0200

    test: fixed and added layers model unit tests

--- tests/unit/layers/models/layersManager.spec.ts
+++ tests/unit/layers/models/layersManager.spec.ts
@@ -1,18 +1,29 @@
 import config from 'config';
-import { mockLayer } from '../../../../src/common/data/mock/mockLayer';
 import { IMapProxyConfig } from '../../../../src/common/interfaces';
 import { LayersManager } from '../../../../src/layers/models/layersManager';
 import { ConfilctError } from '../../../../src/common/exceptions/http/confilctError';
+import { mockLayerNameAlreadyExists } from '../../mock/mockLayerNameAlreadyExists';
+import { mockLayerNameIsNotExists } from '../../mock/mockLayerNameIsNotExists';
+import * as utils from '../../../../src/common/utils';
 
 let layersManager: LayersManager;
-
+let convertYamlToJsonstub: jest.SpyInstance;
+let convertJsonToYamlStub: jest.SpyInstance;
+let replaceYamlContentStub: jest.SpyInstance;
 const mapproxyConfig = config.get<IMapProxyConfig>('mapproxy');
 describe('layersManager', () => {
   beforeEach(function () {
-    layersManager = new LayersManager({ log: jest.fn() }, { yamlFilePath: mapproxyConfig.yamlFilePath, cache: mapproxyConfig.cache });
+    layersManager = new LayersManager({ log: jest.fn() }, { yamlFilePath: 'tests/unit/mock/mockContent.yaml', cache: mapproxyConfig.cache });
+    // stub util functions
+    convertYamlToJsonstub = jest.spyOn(utils, 'convertYamlToJson');
+    convertJsonToYamlStub = jest.spyOn(utils, 'convertJsonToYaml');
+    replaceYamlContentStub = jest.spyOn(utils, 'replaceYamlFileContent').mockReturnValueOnce(undefined);
+  });
+  afterEach(() => {    
+    jest.clearAllMocks();
   });
   describe('#getLayer', () => {
-    it('return the resource of id 1', function () {
+    it('return the layer of id 1', function () {
       // action
       const resource = layersManager.getLayer();
       // expectation
@@ -25,20 +36,22 @@ describe('layersManager', () => {
   });
   describe('#addLayer', () => {
     it('should reject with conflict error', function () {
-      // mock
-      layersManager.isLayerNameExists = jest.fn().mockReturnValue(true);
       // action
-      const action = () => layersManager.addLayer(mockLayer);
+      const action = () => layersManager.addLayer(mockLayerNameAlreadyExists);
       // expectation
       expect(action).toThrow(ConfilctError);
+      expect(convertYamlToJsonstub).toHaveBeenCalledTimes(1);
+      expect(convertJsonToYamlStub).not.toHaveBeenCalled();
+      expect(replaceYamlContentStub).not.toHaveBeenCalled();
     });
     it('should successfully add layer', function () {
-      // mock
-      layersManager.isLayerNameExists = jest.fn().mockReturnValue(false);
       // action
-      const action = () => layersManager.addLayer(mockLayer);
+      const action = () => layersManager.addLayer(mockLayerNameIsNotExists);
       // expectation
       expect(action).not.toThrow();
+      expect(convertYamlToJsonstub).toHaveBeenCalledTimes(1);
+      expect(convertJsonToYamlStub).toHaveBeenCalledTimes(1);
+      expect(replaceYamlContentStub).toHaveBeenCalledTimes(1);
     });
   });
 });
