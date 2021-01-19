1751547773894d8a8526113ca9567e7df819fd07
shlomiko <cl-shlomikoncha@rnd-hub.com>
2021-01-19 11:45:54 +0200

    test: added mock data for layers model unit tests

---
+++ tests/unit/mock/mockLayerNameAlreadyExists.ts
@@ -0,0 +1,10 @@
+import { ILayerPostRequest } from '../../../src/common/interfaces';
+
+//TODO: remove mock data after contollers implementaion
+export const mockLayerNameAlreadyExists: ILayerPostRequest = {
+  id: 1,
+  name: 'NameIsAlreadyExists',
+  tilesPath: '/path/to/s3/directory/tile',
+  maxZoomLevel: 18,
+  description: 'amsterdam 5m layer discription',
+};
