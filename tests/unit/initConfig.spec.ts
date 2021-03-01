613e9d39759a792c2e9638d312f856d9d4057bd4
shlomiko <cl-shlomikoncha@rnd-hub.com>
2021-03-01 13:22:49 +0200

    test: fixed tests

--- tests/unit/initConfig.spec.ts
+++ tests/unit/initConfig.spec.ts
@@ -1,8 +1,10 @@
 import fs from 'fs';
 import config from 'config';
+import { container } from 'tsyringe';
 import * as utils from '../../src/common/utils';
 import { initConfig } from '../../src/initConfig';
 import { IMapProxyConfig } from '../../src/common/interfaces';
+import { Services } from '../../src/common/constants';
 
 let writeFileSyncSpy: jest.SpyInstance;
 let existsSyncSpy: jest.SpyInstance;
@@ -12,14 +14,16 @@ const mapproxyConfig = config.get<IMapProxyConfig>('mapproxy');
 describe('initConfig', () => {
   beforeEach(function () {
     // stub functions
+    container.register(Services.MAPPROXY, { useValue: mapproxyConfig });
+    container.register(Services.LOGGER, { useValue: { log: jest.fn() } });
     writeFileSyncSpy = jest.spyOn(fs, 'writeFileSync').mockReturnValue(undefined);
     convertJsonToYamlSpy = jest.spyOn(utils, 'convertJsonToYaml');
     existsSyncSpy = jest.spyOn(fs, 'existsSync');
   });
   afterEach(() => {
+    container.reset();
     jest.clearAllMocks();
   });
-
   describe('#convertYamlToJson', () => {
     it('should create default config file if not exists', function () {
       // mock
