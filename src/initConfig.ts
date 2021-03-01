b58eb698c8b2846cbb236dc7c1594e6715cb6bd2
shlomiko <cl-shlomikoncha@rnd-hub.com>
2021-03-01 11:40:13 +0200

    fix: fixed creation of default config file

--- src/initConfig.ts
+++ src/initConfig.ts
@@ -6,34 +6,32 @@ import { IMapProxyConfig, IMapProxyJsonDocument } from './common/interfaces';
 import { convertJsonToYaml, convertYamlToJson } from './common/utils';
 
 const mapproxyConfig = config.get<IMapProxyConfig>('mapproxy');
-
 // create default config yaml file if not exists
 export function initConfig(yamlFilePath: string, endPointUrl: string, bucket: string): void {
   try {
     const extension: string = extname(yamlFilePath);
     if (extension !== mapproxyConfig.fileExt) {
-      throw new ServiceUnavailableError(`Unsupported extension: '${extension}' , must be '${mapproxyConfig.fileExt as string}'`);
+      throw new ServiceUnavailableError(
+        `Invalid file path or Unsupported extension, expected to receive file path with: '${mapproxyConfig.fileExt as string}' extension`
+      );
     }
     if (existsSync(yamlFilePath) && extension === mapproxyConfig.fileExt) {
-      console.log('using existing config file');
+      // TODO: add logger
+      // logger.log('info', 'Using existing configuration file');
       return;
     }
-
-    console.log('create config file');
+    // TODO: add logger
+    // logger.log('info', 'Configuration file not found, create default configuration file');
     const jsonDocument: IMapProxyJsonDocument = convertYamlToJson(mapproxyConfig.defaultFilePath as string);
     jsonDocument.globals.cache.s3.endpoint_url = endPointUrl;
     jsonDocument.globals.cache.s3.bucket_name = bucket;
     const yamlContent = convertJsonToYaml(jsonDocument);
     writeFileSync(yamlFilePath, yamlContent, 'utf8');
+    // TODO: add logger
+    // logger.log('info', 'Successfully created default configuration file')
   } catch (error) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    if (error.code === 'ENOENT') {
-      throw new ServiceUnavailableError(`Invalid file path: '${yamlFilePath}'`);
-    }
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    if (error.code === 'EISDIR') {
-      throw new ServiceUnavailableError(`'${yamlFilePath}' is a directory, expected to receive file path`);
-    }
+    // TODO: add logger
+    // logger.log('error', `Error occurred: ${error}`);
     throw new Error(error);
   }
 }
