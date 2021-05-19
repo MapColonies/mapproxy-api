5252d325d15f447d776c6875860a73c73737ec97
CL-SHLOMIKONCHA <cl_shlomikoncha@rnd-hub.com>
2021-05-19 12:49:16 +0300

    test: added test for utils

--- tests/unit/layers/common/utils.spec.ts
+++ tests/unit/layers/common/utils.spec.ts
@@ -7,6 +7,7 @@ let safeLoadStub: jest.SpyInstance;
 let safeDumpStub: jest.SpyInstance;
 let sortArrayByZIndexStub: jest.SpyInstance;
 let replaceYamlFileContentStub: jest.SpyInstance;
+
 describe('utils', () => {
   beforeEach(function () {
     // stub util functions
@@ -15,10 +16,12 @@ describe('utils', () => {
     sortArrayByZIndexStub = jest.spyOn(utils, 'sortArrayByZIndex');
     replaceYamlFileContentStub = jest.spyOn(utils, 'replaceYamlFileContent');
   });
+
   afterEach(() => {
     jest.resetAllMocks();
     jest.restoreAllMocks();
   });
+
   describe('#convertYamlToJson', () => {
     it('should convert yaml content to json object', function () {
       // mock
@@ -29,6 +32,7 @@ describe('utils', () => {
       expect(typeof action()).toBe('object');
       expect(safeLoadStub).toHaveBeenCalledTimes(1);
     });
+
     it('should reject with file not found or not exists', function () {
       // mock
       const mockNotExistsYamlFile = 'tests/unit/mock/notFound.yaml';
@@ -38,6 +42,7 @@ describe('utils', () => {
       expect(action).toThrow(ServiceUnavailableError);
       expect(safeLoadStub).not.toHaveBeenCalled();
     });
+
     it('should reject with invalid yaml syntax', function () {
       // mock
       const invalidYamlSyntaxFile = 'tests/unit/mock/mockInvalidYamlSyntax.yaml';
@@ -48,6 +53,7 @@ describe('utils', () => {
       expect(safeLoadStub).toHaveBeenCalledTimes(1);
     });
   });
+
   describe('#convertJsonToYaml', () => {
     it('should convert json object content to yaml content', function () {
       // mock
@@ -60,6 +66,7 @@ describe('utils', () => {
       expect(safeDumpStub).toHaveBeenCalledTimes(1);
     });
   });
+
   describe('#replaceYamlFileContent', () => {
     it('should replace file content with the requested yaml content', function () {
       // mock
@@ -69,21 +76,25 @@ describe('utils', () => {
         name: 'mockLayer',
         sources: ['mockSource'],
       };
+
       expect(mockConvertedJson.layers).not.toContainEqual(mockLayer);
       mockConvertedJson.layers.push(mockLayer);
+
       // action
       const convertedYaml: string = utils.convertJsonToYaml(mockConvertedJson);
       const newConvertedJson: IMapProxyJsonDocument = jsyaml.safeLoad(convertedYaml) as IMapProxyJsonDocument;
       replaceYamlFileContentStub.mockReturnValueOnce(undefined);
       const action = () => utils.replaceYamlFileContent(mockYamlFile, convertedYaml);
+
       // expectation
       expect(action).not.toThrow();
       expect(typeof convertedYaml).toBe('string');
       expect(newConvertedJson.layers).toContainEqual(mockLayer);
     });
   });
+
   describe('#sortArrayByZIndex', () => {
-    test('should sort an array in numerical order', function () {
+    it('should sort an array in numerical order', function () {
       // mock
       const layers: IMosaicLayerObject[] = [
         { layerName: 'mockLayer1', zIndex: 2 },
@@ -92,6 +103,7 @@ describe('utils', () => {
       ];
       // action
       const action = () => utils.sortArrayByZIndex(layers);
+
       // expectation
       expect(layers[0].layerName).toBe('mockLayer1');
       expect(layers[1].layerName).toBe('mockLayer2');
@@ -99,6 +111,28 @@ describe('utils', () => {
       expect(action()).toEqual(['mockLayer3', 'mockLayer2', 'mockLayer1']);
       expect(sortArrayByZIndexStub).toHaveReturned();
       expect(sortArrayByZIndexStub).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('#getFileExtension', () => {
+    it('should return the file extension from a path', function () {
+      // mock
+      const path = '/path/to/file.gpkg';
+      // action
+      const action = () => utils.getFileExtension(path);
+
+      // expectation
+      expect(action()).toEqual('.gpkg');
+    });
+
+    it('should return the empty string if the path does not contain file extension', function () {
+      // mock
+      const path = '/path/to/dir/';
+      // action
+      const action = () => utils.getFileExtension(path);
+
+      // expectation
+      expect(action()).toEqual('');
     });
   });
 });
