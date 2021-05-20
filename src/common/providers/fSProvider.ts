0375c15adb5745119d5563ab5ecd28aa22ca3353
CL-SHLOMIKONCHA <cl_shlomikoncha@rnd-hub.com>
2021-05-20 08:38:03 +0300

    chore: removed unnecessary dependencies values

--- src/common/providers/fSProvider.ts
+++ src/common/providers/fSProvider.ts
@@ -3,13 +3,6 @@ import { Services } from '../constants';
 import { IFileProvider, ILogger, IMapProxyConfig } from '../interfaces';
 
 export class FSProvider implements IFileProvider {
-  private readonly logger: ILogger;
-  private readonly mapproxyConfig: IMapProxyConfig;
-
-  public constructor(container: DependencyContainer) {
-    this.logger = container.resolve(Services.LOGGER);
-    this.mapproxyConfig = container.resolve(Services.MAPPROXY);
-  }
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   public async uploadFile(filePath: string): Promise<void> {
     return Promise.resolve();
