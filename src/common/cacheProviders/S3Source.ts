9178b8b13b390169527c3f8e714ef8bbcfbf56a3
Shlomi k <65117898+CL-SHLOMIKONCHA@users.noreply.github.com>
2021-07-18 14:23:05 +0300

    chore: adjust tiles path by source cache type (#71)

--- src/common/cacheProviders/S3Source.ts
+++ src/common/cacheProviders/S3Source.ts
@@ -3,7 +3,7 @@ import { DependencyContainer } from 'tsyringe';
 import { SourceTypes } from '../enums/sourceTypes';
 import { Services } from '../constants';
 import { ICacheProvider, IMapProxyConfig, IS3Source } from '../interfaces';
-import { getTilesPath } from '../utils';
+import { adjustTilesPath } from '../utils';
 
 class S3Source implements ICacheProvider {
   private readonly mapproxyConfig: IMapProxyConfig;
@@ -13,9 +13,10 @@ class S3Source implements ICacheProvider {
   }
 
   public getCacheSource(sourcePath: string): IS3Source {
+    const sourceCacheType = SourceTypes.S3;
     const s3Source: IS3Source = {
-      type: SourceTypes.S3,
-      directory: getTilesPath(sourcePath),
+      type: sourceCacheType,
+      directory: adjustTilesPath(sourcePath, sourceCacheType),
       directory_layout: this.mapproxyConfig.cache.directoryLayout,
     };
 
